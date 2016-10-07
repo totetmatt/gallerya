92b5ed3ed6a89cd3dcdc2cafe859eacf8e79b466
totetmatt <matthieu.totet@gmail.com>
2016-10-08 01:15:51 +0200

    Code refactor

--- gallerya.go
+++ gallerya.go
@@ -9,36 +9,57 @@ import (
     "github.com/flosch/pongo2"
 
 )
-var dir_thumb = "./thumb"
-var dir_original = "./original"
+    
+/** Configuration Struct >>**/
+type GalleryaConfiguration struct {
+    thumb_directory string
+    original_directory  string
+}
+    
+
+func (config *GalleryaConfiguration) preCheck() {
+    r,_ := fexists(config.thumb_directory)
+    if(!r) {
+        os.MkdirAll(config.thumb_directory, os.ModePerm)
+    }
+}
+
+func (config *GalleryaConfiguration) thumb_file(filename string) string {
+    return filepath.Join(config.thumb_directory,filename)   
+}
+
+func (config *GalleryaConfiguration) original_file(filename string) string {
+    return filepath.Join(config.original_directory,filename)   
+}
+/**<< Configuration Struct **/
 
 var templateIndex = pongo2.Must(pongo2.FromFile("./index.template"))
 
-func worker(id int, jobs <-chan string, results chan<- string) {
+func worker(config *GalleryaConfiguration, jobs <-chan string, results chan<- string) {
     for j := range jobs {
-        fmt.Println(">> worker", id, "processing job", j)
-        do_thumb(j)
+        fmt.Println("Processing image", j)
+        do_thumb(j,config)
         results <- j
     }
 }
 
-func thumb_from_dir(path string)  {
+func thumb_from_dir(config *GalleryaConfiguration)  {
     
     jobs := make(chan string, 1000)
     results := make(chan string, 1000)
 
     for w := 1; w <= 8; w++ {
-        go worker(w, jobs, results)
+        go worker(config, jobs, results)
     }
   
     
-    files, _ := ioutil.ReadDir(path)
+    files, _ := ioutil.ReadDir(config.original_directory)
     for _, f := range files {
-            jobs <- filepath.Join(path,f.Name())   
+            jobs <- f.Name()
 
     }
     close(jobs)
-    generate_html(files)
+    generate_html(files,config)
     
     for a := 1; a <=  len(files); a++ {
         r := <-results
@@ -48,21 +69,20 @@ func thumb_from_dir(path string)  {
 
 }
 
-func generate_html(files []os.FileInfo) {
+func generate_html(files []os.FileInfo, config *GalleryaConfiguration) {
     f, _ := os.Create("./index.html")
     defer f.Close()
 
-    err := templateIndex.ExecuteWriter(pongo2.Context{"files": files,"dir_original":dir_original,"dir_thumb":dir_thumb}, f)
+    err := templateIndex.ExecuteWriter(pongo2.Context{"files": files,"dir_original":config.original_directory,"dir_thumb":config.thumb_directory}, f)
     if err != nil {
         panic(err)
     }
 }
 
-func do_thumb(path string) {
-    img, err := imaging.Open(path)
+func do_thumb(file string, config *GalleryaConfiguration) {
+    img, err := imaging.Open(config.original_file(file))
     dst := imaging.Fill(img, 360, 247, imaging.Center, imaging.Lanczos)
-    _,file := filepath.Split(path)
-    err2 := imaging.Save(dst, filepath.Join(dir_thumb,file))
+    err2 := imaging.Save(dst, config.thumb_file(file))
     if err2 != nil {
         panic(err)
     }
@@ -76,11 +96,11 @@ func fexists(path string) (bool, error) {
 }
 
 func main() {
+    config := GalleryaConfiguration{}
 
+    config.thumb_directory = "./thumb"
+    config.original_directory = "./original"
 
-    r,_ := fexists(dir_thumb)
-    if(!r) {
-        os.MkdirAll(dir_thumb, os.ModePerm)
-    }
-    thumb_from_dir(dir_original)
+    config.preCheck()
+    thumb_from_dir(&config)
 }
