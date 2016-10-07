811bd899cdfd120abe7857639143ff78bae074be
totetmatt <matthieu.totet@gmail.com>
2016-10-07 19:44:44 +0200

    Worker pattern

--- gallerya.go
+++ gallerya.go
@@ -4,7 +4,6 @@ import (
     "github.com/disintegration/imaging"
     "io/ioutil"
     "fmt"
-    "sync"
     "os"
     "path/filepath"
 
@@ -12,21 +11,41 @@ import (
 var dir_thumb = "./thumb"
 var dir_original = "./original"
 
-var wg sync.WaitGroup
-
-func thumb_from_dir(path string)  {
-    files, _ := ioutil.ReadDir(path)
-    wg.Add(len(files))
-    for _, f := range files {
-            go do_thumb(path,f.Name()) // TODO : Evole to worker https://gobyexample.com/worker-pools
-            
+func worker(id int, jobs <-chan string, results chan<- string) {
+    for j := range jobs {
+        fmt.Println(">> worker", id, "processing job", j)
+        do_thumb(j)
+        results <- j
     }
 }
-func do_thumb(path string,file string) {
-    defer wg.Done()
-    fmt.Println(filepath.Join(path, file))
-    img, err := imaging.Open(filepath.Join(path, file))
+
+func thumb_from_dir(path string)  {
+    
+    jobs := make(chan string, 1000)
+    results := make(chan string, 1000)
+
+    for w := 1; w <= 8; w++ {
+        go worker(w, jobs, results)
+    }
+  
+    
+    files, _ := ioutil.ReadDir(path)
+    for _, f := range files {
+            jobs <- filepath.Join(path,f.Name())    
+    }
+    close(jobs)
+    
+
+    for a := 1; a <=  len(files); a++ {
+        r := <-results
+        fmt.Println("Finished "+r)
+    }
+}
+
+func do_thumb(path string) {
+    img, err := imaging.Open(path)
     dst := imaging.Fill(img, 100, 100, imaging.Center, imaging.Lanczos)
+    _,file := filepath.Split(path)
     err2 := imaging.Save(dst, filepath.Join(dir_thumb,file))
     if err2 != nil {
         panic(err)
@@ -42,11 +61,8 @@ func fexists(path string) (bool, error) {
 
 func main() {
     r,_ := fexists(dir_thumb)
-    if(r) {
-        fmt.Println(filepath.Join(dir_thumb, "file"))
-    } else {
+    if(!r) {
         os.MkdirAll(dir_thumb, os.ModePerm)
     }
     thumb_from_dir(dir_original)
-    wg.Wait()
 }
